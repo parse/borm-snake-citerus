5958ca739bb782dfaf74209e99a8428ba4bcaaa7
Anders Hassis <anders.hassis@gmail.com>
2011-05-03 13:29:24 +0200

    meh

---
+++ java-brain/se/citerus/crazysnake/brain/BormBrain.java
@@ -0,0 +1,31 @@
+package se.citerus.crazysnake.brain;
+
+import java.util.Set;
+
+import se.citerus.crazysnake.Brain;
+import se.citerus.crazysnake.GameState;
+import se.citerus.crazysnake.HeatMeta;
+import se.citerus.crazysnake.Movement;
+
+class BormBrain implements Brain {
+	Movement getNextMove() {
+		return null;
+		//LOL
+	}
+	
+	public String getName() {
+		return "Team BORM";
+	}
+
+	@Override
+	public Movement getNextMove(GameState arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(Set<String> arg0, HeatMeta arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
